9c7b4affeccaf655b6ba799d8173f3e7056cda31
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-12-27 00:45:15 +0530

    deploy testing

--- verceldjango/settings.py
+++ verceldjango/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') #correcting static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #correcting static files
 STATICFILES_DIRS = os.path.join(BASE_DIR , 'static'),
